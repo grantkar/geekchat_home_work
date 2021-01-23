8a1f285cc66d6afbfe1d736d0660a60f6f997e1d
grantkar <hrant87@inbox.ru>
2021-01-23 15:39:13 +0400

    Lesson7_HW7_GeekChat

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,70 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+    private ServerSocket server;
+    private Socket socket;
+    private final int PORT = 8189;
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public Server() {
+        clients = new CopyOnWriteArrayList<>();
+        authService = new SimpleAuthService();
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Client connected");
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(ClientHandler clientHandler, String msg){
+        String message = String.format("[ %s ]: %s", clientHandler.getNickname(), msg);
+
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+    public void privMassege(ClientHandler clientHandler, String nick,  String privMsg){
+        String message = String.format("[ %s ]: %s", clientHandler.getNickname(), privMsg);
+        clientHandler.sendMsg(message);
+        for (ClientHandler client : clients) {
+            if (client.getNickname().equals(nick)){
+                client.sendMsg(message);
+            }
+
+        }
+    }
+
+    void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+}
